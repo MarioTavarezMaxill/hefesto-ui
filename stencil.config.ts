bf653ddc7e2ad73fcd9c10efdb80155aca87a897
Mario Tavarez <121114433+MarioTavarezMaxill@users.noreply.github.com>
2024-07-09 12:35:46 -0600

    chore: update npmignore, package-lock.json, package.json, postcss.config.js, and stencil.config.ts (#8)

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,13 +2,13 @@ import { Config } from '@stencil/core';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
 import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export const config: Config = {
-	namespace: 'hefesto-ui',
-	plugins: [
-		tailwind(),
-		tailwindHMR(),
+  namespace: 'hefesto-ui',
+  plugins: [
+    tailwind(),
+    tailwindHMR(),
     postcss({
       plugins: [
         require('postcss-import'),
@@ -16,37 +16,48 @@ export const config: Config = {
         autoprefixer(),
       ],
     }),
-	],
-	outputTargets: [
-		{
-			type: 'dist',
-			esmLoaderPath: '../loader',
-		},
-
-		{
-			type: 'dist-custom-elements',
-			customElementsExportBehavior: 'auto-define-custom-elements',
-			externalRuntime: false,
-		},
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      dir: 'dist',
+      buildDir: 'build',
+    },
     {
       type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
       generateTypeDeclarations: true,
     },
-		{
-			type: 'docs-readme',
-		},
-		{
-			type: 'www',
-			serviceWorker: null,
-		},
-	],
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null,
+      copy: [
+        { src: '**/*.html' },
+        { src: '**/*.js' },
+        { src: '**/*.css' },
+        { src: '**/*.json' },
+        { src: '**/*.d.ts' },
+        { src: '**/*.map', dest: 'ignore', warn: false },
+        { src: '**/*.stories.*', dest: 'ignore', warn: false },
+        { src: '**/stats.html', dest: 'ignore', warn: false },
+        { src: '**/collection/**', dest: 'ignore', warn: false },
+      ]
+    },
+  ],
   rollupPlugins: {
     after: [
-      nodePolyfills(),
-    ]
+      visualizer({ filename: './dist/stats.html' }),
+    ],
   },
-	testing: {
-		browserHeadless: 'new',
-	},
+  minifyJs: true,
+  minifyCss: true,
+  hashFileNames: true,
+  testing: {
+    browserHeadless: 'new',
+  },
 };
-
