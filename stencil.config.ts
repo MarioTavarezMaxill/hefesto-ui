edd7bc1c9b6460703de937497eca23ec45055a0c
Mario Tavarez <mjtavarez@maxillc.com>
2024-07-09 16:16:10 -0600

    chore: update bundle size badge in README.md

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,13 +2,13 @@ import { Config } from '@stencil/core';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
 import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export const config: Config = {
-  namespace: 'hefesto-ui',
-  plugins: [
-    tailwind(),
-    tailwindHMR(),
+	namespace: 'hefesto-ui',
+	globalStyle: 'www/tailwind.css',
+	plugins: [
+		tailwind(),
+		tailwindHMR(),
     postcss({
       plugins: [
         require('postcss-import'),
@@ -16,48 +16,27 @@ export const config: Config = {
         autoprefixer(),
       ],
     }),
-  ],
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-      dir: 'dist',
-      buildDir: 'build',
-    },
-    {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-      generateTypeDeclarations: true,
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null,
-      copy: [
-        { src: '**/*.html' },
-        { src: '**/*.js' },
-        { src: '**/*.css' },
-        { src: '**/*.json' },
-        { src: '**/*.d.ts' },
-        { src: '**/*.map', dest: 'ignore', warn: false },
-        { src: '**/*.stories.*', dest: 'ignore', warn: false },
-        { src: '**/stats.html', dest: 'ignore', warn: false },
-        { src: '**/collection/**', dest: 'ignore', warn: false },
-      ]
-    },
-  ],
-  rollupPlugins: {
-    after: [
-      visualizer({ filename: './dist/stats.html' }),
-    ],
-  },
-  minifyJs: true,
-  minifyCss: true,
-  hashFileNames: true,
-  testing: {
-    browserHeadless: 'new',
-  },
+	],
+	outputTargets: [
+		{
+			type: 'dist',
+			esmLoaderPath: '../loader',
+		},
+
+		{
+			type: 'dist-custom-elements',
+			customElementsExportBehavior: 'auto-define-custom-elements',
+			externalRuntime: false,
+		},
+		{
+			type: 'docs-readme',
+		},
+		{
+			type: 'www',
+			serviceWorker: null,
+		},
+	],
+	testing: {
+		browserHeadless: 'new',
+	},
 };
